7ffdd540e6a1697dd1fcd9fbe4cf4cdbf3bc2aaf
matheusapc22 <matheusapc22@gmail.com>
2026-01-20 14:50:04 -0300

    Remove share and city actions from panel header

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -8,9 +8,9 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex flex-col flex-1">
         <Topbar />
-        <main className="flex flex-1">
+        <main className="flex flex-1 min-h-0">
           <FilterPanel />
-          <div className="flex-1 bg-gray-100">
+          <div className="flex-1 bg-gray-100 min-h-0 relative overflow-hidden">
             {children}
           </div>
         </main>
